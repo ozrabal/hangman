e584e7ae08fd80d65fa8a5e7620d83777b95bc50
Piotr Łepkowski <ozrabal@gmail.com>
2017-12-06 00:35:06 +0100

    base saga scaffold

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,21 @@
 import { applyMiddleware, createStore, compose } from 'redux'
-// import createSagaMiddleware from 'redux-saga'
+import createSagaMiddleware from 'redux-saga'
 import { createLogger } from 'redux-logger'
 
 import rootReducer from '../reducers'
+import rootSaga from '../sagas'
 
 export default function configureStore(preloadedState) {
+  const sagaMiddleware = createSagaMiddleware()
   const logger = createLogger({ collapsed: true })
   const store = createStore(
     rootReducer,
     preloadedState,
     compose(
-      applyMiddleware(logger),
+      applyMiddleware(sagaMiddleware, logger),
       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (noop) => noop,
     )
   )
+  sagaMiddleware.run(rootSaga)
   return store
 }
