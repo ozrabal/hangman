51bd930e6ed9f4da853be9d0a5cc8be8d27913be
Piotr Łepkowski <ozrabal@gmail.com>
2017-12-11 00:02:12 +0100

    key doen event handler

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
-import { requestWord } from './actions/game'
+import { requestWord, keyPressed } from './actions/game'
 import Word from './containers/word'
 
 import styles from './styles/index.css'
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.onKeyDown = this.onKeyDown.bind(this)
+  }
+
   componentDidMount() {
     const { actions } = this.props
+    document.addEventListener('keydown', this.onKeyDown, false)
 
     actions.requestWord()
+  }
+
+  onKeyDown(event) {
+    const { actions } = this.props
+    actions.keyPressed(event.key)
   }
 
   render() {
@@ -33,7 +44,7 @@ App.propTypes = {
 App.defaultProps = {}
 
 const mapDispatchToProps = (dispatch) => {
-  const actions = Object.assign({}, { requestWord })
+  const actions = Object.assign({}, { requestWord, keyPressed })
   return {
     actions: bindActionCreators(actions, dispatch),
   }
