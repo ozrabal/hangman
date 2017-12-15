b36851ab2304505cf2bf89f8c37c143a97ecc480
Piotr Łepkowski <ozrabal@gmail.com>
2017-12-16 00:09:43 +0100

    missed letter saga

--- src/sagas/game.js
+++ src/sagas/game.js
@@ -1,15 +1,15 @@
 import { takeLatest, put, take, cancel, call, select } from 'redux-saga/effects'
 import { LOCATION_CHANGE } from 'react-router-redux'
 import { getWord, getLetter } from '../selectors'
-import { types, receivedWord, errorReceivingWord, checkLetter, checkWin } from '../actions/game'
+import { types, receivedWord, errorReceivingWord, checkLetter, checkWin, setMissedLetter } from '../actions/game'
 import request from '../api/request'
 import endpoints from '../api/endpoints'
 
 export function* loadWord() {
   try {
     const response = yield call(request, endpoints.GET_WORD, { method: 'GET' })
-    response.word = response.word.split('')
-    const masked = response.word.map((letter) => ({ letter: letter.toUpperCase(), masked: true }))
+    response.word = response.word.toUpperCase().split('')
+    const masked = response.word.map((letter) => ({ letter, masked: true }))
     yield put(receivedWord(masked))
   } catch (error) {
     yield put(errorReceivingWord(error))
@@ -18,11 +18,17 @@ export function* loadWord() {
 
 export function* makeCheckLetter() {
   const word = yield select(getWord)
-  const letter = yield select(getLetter)
+  let letter = yield select(getLetter)
+  letter = letter.toUpperCase()
   const unmasked = word.map((a) => (
-    a.letter === letter.toUpperCase() ? { letter: a.letter, masked: false } : { letter: a.letter, masked: a.masked }
+    a.letter === letter ? { letter: a.letter, masked: false } : { letter: a.letter, masked: a.masked }
   ))
+
   yield put(checkLetter(unmasked))
+  const indices = word.findIndex((index) => index.letter === letter)
+  if (indices < 0) {
+    yield put(setMissedLetter(letter))
+  }
 }
 
 export function* makeCheckWin() {
